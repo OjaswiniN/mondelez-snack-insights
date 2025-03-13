655b4a959b9e39d7245befa4dc76759d818b99e6
Ojaswini Nandwana <nandwanaojaswini@gmail.com>
2025-03-14 02:04:50 +0530

    Added common and annotatoin classes

---
+++ src/main/java/com/mondelez/annotation/CurrentUser.java
@@ -0,0 +1,11 @@
+/* (C) 2023 Mondelez Insights */
+package com.mondelez.annotation;
+
+import java.lang.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {}
---
+++ src/main/java/com/mondelez/common/Roles.java
@@ -0,0 +1,8 @@
+/* (C) 2023 Mondelez Insights */
+package com.mondelez.common;
+
+public enum Roles {
+  ROLE_GUEST,
+  ROLE_USER,
+  ROLE_ADMIN;
+}
